56e4726ca158ca9964059b55cfa2a74be6bc9c0a
tonnycoder001 <tonnykahihia@gmail.com>
2024-07-28 13:43:11 +0300

    Create payment functions

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaypalController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Paypal
+Route::post('paypal', [PaypalController::class, 'payment'])->name('paypal');
+Route::get('paypal.success', [PaypalController::class, 'success'])->name('paypal_success');
+Route::get('paypal.cancel', [PaypalController::class, 'cancel'])->name('paypal_cancel');
