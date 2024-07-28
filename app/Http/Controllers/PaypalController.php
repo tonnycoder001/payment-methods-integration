11dd844d4ac4efb333f36d83e699aff144841d68
tonnycoder001 <tonnykahihia@gmail.com>
2024-07-28 13:42:37 +0300

    Integrate paypal

---
+++ app/Http/Controllers/PaypalController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Srmklive\PayPal\Services\PayPal as PayPalClient;
+
+class PaypalController extends Controller
+{
+    public function payment(Request $request)
+    {
+        $provider = new PayPalClient;
+        $provider->setApiCredentials(config('paypal'));
+        $paypalToken = $provider->getAccessToken();
+
+        $response = $provider->createOrder([
+            "intent" => "CAPTURE",
+            "application_context" => [
+                "return_url" => route('paypal_success'),
+                "cancel_url" => route('paypal_cancel'),
+            ],
+            "purchase_units" => [
+                [
+                    "amount" => [
+                        "currency_code" => "USD",
+                        "value" => $request->price
+                    ]
+                ]
+            ]
+        ]);
+        // dd($response);
+        if (isset($response['id']) && $response['id'] != null) {
+            // redirect to approve url
+            foreach ($response['links'] as $link) {
+                if ($link['rel'] == 'approve') {
+                    return redirect()->away($link['href']);
+                }
+            }
+        } else {
+            return redirect()->route('paypal_cancel');
+        }
+    }
+
+    public function success(Request $request)
+    {
+        $provider = new PayPalClient;
+        $provider->setApiCredentials(config('paypal'));
+        $paypalToken = $provider->getAccessToken();
+        $response = $provider->capturePaymentOrder($request['token']);
+
+        if (isset($response['status']) && $response['status'] == 'COMPLETED') {
+            // payment was successful, you can process the order now
+            return 'Payment successful.';
+        } else {
+            // payment failed
+            return redirect()->route('paypal_cancel');
+        }
+    }
+
+    public function cancel(Request $request)
+    {
+        return 'Payment cancelled.';
+    }
+}
